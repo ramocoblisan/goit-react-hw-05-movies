00a45594fc562be594f56a54457eee2b33352861
ramocoblisan <ramonaioana67@yahoo.com>
2024-05-18 16:21:38 +0300

    SharedLayout updated

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './SharedLayout.module.css';
-import { RiMovie2Line } from "react-icons/ri";
 import { Outlet } from "react-router-dom";
 
 const NavigationBar = () => {
